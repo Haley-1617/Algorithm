304d8fc1a8006b7502c98d3e10effc4026244d3f
Haley-1617 <alicelai2920@gmail.com>
2020-02-23 19:26:32 -0800

    Initial Commit

---
+++ Algorithm_Problems/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithm_Problems
+//
+//  Created by Haley Lai on 2/23/20.
+//  Copyright © 2020 Haley Lai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
